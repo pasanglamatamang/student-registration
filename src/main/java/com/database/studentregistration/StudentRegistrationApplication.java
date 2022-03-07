51edbdf2b7b7d20f9076c1e45ebede347edf47d3
Pasang Lama Tamang <pasang.lama.tamang11@gmail.com>
2022-03-07 09:08:42 -0800

    Added swagger2 V3.0.0 and fixed the issue 'Failed to start bean 'documentationPluginsBootstrapper' by adding pathmatch to application.yml

--- src/main/java/com/database/studentregistration/StudentRegistrationApplication.java
+++ src/main/java/com/database/studentregistration/StudentRegistrationApplication.java
@@ -2,8 +2,10 @@ package com.database.studentregistration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class StudentRegistrationApplication {
 
 	public static void main(String[] args) {
