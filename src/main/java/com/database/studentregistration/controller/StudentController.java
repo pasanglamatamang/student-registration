430cd3bd8b4fd714f455a2b29bc9a64e53a4f079
Pasang Lama Tamang <pasang.lama.tamang11@gmail.com>
2022-03-10 14:38:57 -0800

    Added cross orign in in StudentController.java

--- src/main/java/com/database/studentregistration/controller/StudentController.java
+++ src/main/java/com/database/studentregistration/controller/StudentController.java
@@ -13,7 +13,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
-
+@CrossOrigin(origins = "http://localhost:4200", allowedHeaders = "*")
 public class StudentController {
 
     @Autowired
